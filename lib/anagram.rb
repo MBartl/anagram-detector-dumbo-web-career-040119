f0bc0b6ca7967da5a0ba6675408877894625bd15
Matthew Bartl <mattbartl@gmail.com>
2019-04-08 13:19:14 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,6 +1,16 @@
 # Your code goes here!
 class Anagram
   
+  attr_accessor :word
+  
   def initialize(word)
     @word = word
   end
+  
+  def match(array)
+    array.select do |item|
+      item == word.reverse
+    end
+  end
+  
+end
